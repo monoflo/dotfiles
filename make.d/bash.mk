53e13d054b4c3ea03be8f23f2d963d2383cc51d7
monoflo <16790838+monoflo@users.noreply.github.com>
2022-08-01 09:52:41 -0400

    bash: backup, move conflicting profile sources

---
+++ make.d/bash.mk
@@ -0,0 +1,12 @@
+.PHONY: i-bash u-bash r-bash
+
+TARGETS := ~/.bash_login ~/.bash_profile
+BACKUP  := bak
+
+i-bash:
+	$(foreach rc, $(TARGETS), $(call [ -f $(rc) ] && mv $(rc) $(rc).$(BACKUP); ))
+
+u-bash: i-bash
+
+r-bash:
+	$(foreach rc, $(TARGETS), $(call [ -f $(rc).$(BACKUP) ] && mv $(rc).$(BACKUP) $(rc)))
