72c86638b67727a41f35a2ea57a4fb9dd2cb59b0
monoflo <16790838+monoflo@users.noreply.github.com>
2022-08-01 01:41:00 -0400

    dotfiles.mk: implement restow

--- make.d/dotfiles.mk
+++ make.d/dotfiles.mk
@@ -1,14 +1,14 @@
 .PHONY: i-dotfiles u-dotfiles r-dotfiles
 
 PACKAGE := dotfiles.d
-
 ARGS    := --dotfiles --no-folding -t "$(HOME)"
 
 i-dotfiles:
 	@mkdir -p $(XDG_CONFIG_HOME)
 	stow -S $(ARGS) $(PACKAGE)
 
-u-dotfiles: i-dotfiles
+u-dotfiles:
+	stow -R $(ARGS) $(PACKAGE)
 
 r-dotfiles:
 	stow -D $(ARGS) $(PACKAGE)
