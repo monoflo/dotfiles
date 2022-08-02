b81a6c58e904d12d0ce8f07be4182afb24e4fe41
monoflo <16790838+monoflo@users.noreply.github.com>
2022-08-02 13:33:23 -0400

    bash: don't backup rc files on update

--- make.d/stage-0.d/bash.mk
+++ make.d/stage-0.d/bash.mk
@@ -4,10 +4,9 @@ BASH_TARGETS := ~/.bashrc ~/.bash_login ~/.bash_profile
 BASH_BACKUP  := bak
 
 i-bash:
-
 	$(foreach rc, $(BASH_TARGETS), $(call [ -f $(rc) -a ! -h $(rc) ] && mv $(rc) $(rc).$(BASH_BACKUP); ))
 
-u-bash: i-bash
+u-bash:
 
 r-bash:
 	$(foreach rc, $(BASH_TARGETS), $(call [ -f $(rc).$(BASH_BACKUP) ] && mv $(rc).$(BASH_BACKUP) $(rc)))
