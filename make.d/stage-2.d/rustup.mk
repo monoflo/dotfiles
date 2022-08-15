d00f49354268085a6b632fec051ecd9d4fd3da6f
monoflo <16790838+monoflo@users.noreply.github.com>
2022-08-15 10:12:07 -0400

    rustup: Specify args to streamline, no PATH mods

--- make.d/stage-2.d/rustup.mk
+++ make.d/stage-2.d/rustup.mk
@@ -1,9 +1,11 @@
 .PHONY: i-rustup u-rustup r-rustup
 
+RUSTUP_INSTALL_ARGS := -y --no-modify-path
+
 
 i-rustup:
 ifeq (, $(shell which rustup))
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- $(RUSTUP_INSTALL_ARGS)
 endif
 
 u-rustup:
