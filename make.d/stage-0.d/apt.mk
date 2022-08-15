3351b633baae33303ce683ba8df2508177a6f4dc
monoflo <16790838+monoflo@users.noreply.github.com>
2022-08-15 11:26:10 -0400

    apt: Use apt-get instead of apt

--- make.d/stage-0.d/apt.mk
+++ make.d/stage-0.d/apt.mk
@@ -1,17 +1,17 @@
 .PHONY: i-apt u-apt r-apt
 
-ifneq (, $(shell command -v apt))  # command -v apt
+ifneq (, $(shell command -v apt-get))  # command -v apt-get
 
 APT_PACKAGES := packages.d/apt
 
 
 i-apt:
-	cat $(APT_PACKAGES) | xargs sudo apt install -y
+	cat $(APT_PACKAGES) | xargs sudo apt-get install -y
 
 u-apt: i-apt
-	sudo apt update -y
+	sudo apt-get update -y
 
 r-apt:
-	cat $(APT_PACKAGES) | xargs sudo apt remove -y
+	cat $(APT_PACKAGES) | xargs sudo apt-get remove -y
 
-endif  # command -v apt
+endif  # command -v apt-get
