8f3013f94db1f724fa1c7cd795af188fd50a76db
monoflo <16790838+monoflo@users.noreply.github.com>
2022-08-15 16:40:52 -0400

    shellcheck: Find files inside target

--- check.d/shell.mk
+++ check.d/shell.mk
@@ -1,6 +1,4 @@
 .PHONY: c-shell
 
-CHECK_SHELL_TARGETS := $(shell find dotfiles.d -type f -exec file {} \; | grep script | grep -v zsh | awk -F ': ' '{print $$1}')
-
 c-shell:
-	shellcheck $(CHECK_SHELL_TARGETS)
+	shellcheck $(shell find dotfiles.d -type f -exec file {} \; | grep script | grep -v zsh | awk -F ': ' '{print $$1}')
